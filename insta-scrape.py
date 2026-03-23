bcc6fdcbfb9cf1919ba698154756b60ea16e8558
Otaku  Sensei <nagatopain069@gmail.com>
2026-03-23 14:48:08 +0800

    fix wrong file name

--- insta-scrape.py
+++ insta-scrape.py
@@ -192,7 +192,7 @@ def download(task_id: str):
     return FileResponse(task["file"], filename=task["file"])
 
 
-if __name__ == "__main__":
+if __name__ == "__insta_scrape__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("insta-scrape:app", host="0.0.0.0", port=port)
